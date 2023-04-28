dda5e1439d05d2910cd9edc17b78a6539bba324a
andamiro98 <rlatmdwo9@gmail.com>
2023-04-29 00:47:25 +0900

    project set

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBrP6HFdVHuAZeqiRrK32cAw2tTBSQiY4Q",
+  authDomain: "old-boys-e8097.firebaseapp.com",
+  projectId: "old-boys-e8097",
+  storageBucket: "old-boys-e8097.appspot.com",
+  messagingSenderId: "821719896164",
+  appId: "1:821719896164:web:0c006770bb06cdc379cf71"
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
