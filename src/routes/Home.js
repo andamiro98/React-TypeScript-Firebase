8133f8acab349e6fc68583eb27f36d030db6ca47
andamiro98 <rlatmdwo9@gmail.com>
2023-05-02 02:28:39 +0900

    firebase cloud firestore get data

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { dbService } from 'fbase';
-import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
+import {
+  collection,
+  addDoc,
+  serverTimestamp,
+  getDocs,
+  query,
+} from 'firebase/firestore';
 
 const Home = () => {
   const [post, setPost] = useState('');
+  const [chuweets, setChuweets] = useState([]);
+
+  const getChuweets = async () => {
+    const q = query(collection(dbService, 'chuweets'));
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+      const ChuweetObj = {
+        ...doc.data(),
+        id: doc.id,
+      };
+      console.log(ChuweetObj);
+      setChuweets((prev) => [ChuweetObj, ...prev]);
+    });
+  };
+
+  useEffect(() => {
+    getChuweets();
+  }, []);
 
   const onSubmit = async (e) => {
     e.preventDefault();
