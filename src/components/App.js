1e2fad0096a3fe1776734598f6a32fdfab09c49f
andamiro98 <rlatmdwo9@gmail.com>
2023-05-01 18:31:22 +0900

    Change State Lonin & newAccount

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,31 @@
-import Router from "components/Router";
-import React, { useState } from "react";
-import {authService} from "fbase"
-
+import Router from 'components/Router';
+import React, { useEffect, useState } from 'react';
+import { authService } from 'fbase';
 
 function App() {
-  console.log(authService.currentUser) // user | null
-  
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  console.log(authService.currentUser); // user | null
+
+  const [init, setInit] = useState(false); // firebase initialize
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // onAuthStateChanged Method : 사용자의 로그인 상태의 관리 변화를 관찰하는 메서드
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
 
   return (
     <>
-      <Router isLoggedIn={isLoggedIn}/>
+      {init ? <Router isLoggedIn={isLoggedIn} /> : 'Initializeing...'}
+
       <footer>&copy; {new Date().getFullYear()} Old-boys</footer>
     </>
-    
   );
 }
 
