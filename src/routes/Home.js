59c42c9756666194e056b0bf3a5643d66a59a378
andamiro98 <rlatmdwo9@gmail.com>
2023-05-01 23:42:03 +0900

    Home component create form(input)

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from 'react';
 
 const Home = () => {
-  return <div>
-    Home
-  </div>
-}
+  const [post, setPost] = useState('');
 
-export default Home
+  const onsubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onchange = (e) => {
+    setPost(e.target.value);
+  };
+
+  return (
+    <form onsubmit={onsubmit}>
+      <input
+        type="text"
+        value={post}
+        onChange={onchange}
+        placeholder="추억의 녀석들"
+        maxLength={200}
+      />
+      <input type="submit" value="완료" />
+    </form>
+  );
+};
+
+export default Home;
