58210f918f766902327eccde15f07f94b56622a7
andamiro98 <rlatmdwo9@gmail.com>
2023-05-02 18:03:56 +0900

    onsnapshot

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,6 +6,8 @@ import {
   serverTimestamp,
   getDocs,
   query,
+  orderBy,
+  onSnapshot,
 } from 'firebase/firestore';
 
 const Home = ({ userObj }) => {
@@ -13,21 +15,33 @@ const Home = ({ userObj }) => {
   const [post, setPost] = useState('');
   const [chuweets, setChuweets] = useState([]);
 
-  const getChuweets = async () => {
-    const q = query(collection(dbService, 'chuweets'));
-    const querySnapshot = await getDocs(q);
+  // const getChuweets = async () => {
+  //   const q = query(collection(dbService, 'chuweets'));
+  //   const querySnapshot = await getDocs(q);
 
-    querySnapshot.forEach((doc) => {
-      const ChuweetObj = {
-        ...doc.data(),
-        id: doc.id,
-      };
-      setChuweets((prev) => [ChuweetObj, ...prev]);
-    });
-  };
+  //   querySnapshot.forEach((doc) => {
+  //     const ChuweetObj = {
+  //       ...doc.data(),
+  //       id: doc.id,
+  //     };
+  //     setChuweets((prev) => [ChuweetObj, ...prev]);
+  //   });
+  // };
 
   useEffect(() => {
-    getChuweets();
+    // getChuweets();
+    const q = query(
+      collection(dbService, 'chuweets'),
+      orderBy('createdAt', 'desc')
+    );
+    onSnapshot(q, (snapshot) => {
+      const chuweetArr = snapshot.docs.map((doc) => ({
+        ...doc.data(),
+        postid: doc.id,
+      }));
+      setChuweets(chuweetArr);
+      console.log(chuweetArr);
+    });
   }, []);
 
   const onSubmit = async (e) => {
@@ -60,8 +74,8 @@ const Home = ({ userObj }) => {
       </form>
       <div>
         {chuweets.map((chuweet) => (
-          <div key={chuweet.id}>
-            <h4>{chuweet.post}</h4>
+          <div key={chuweet.postid}>
+            <h4>{chuweet.text}</h4>
           </div>
         ))}
       </div>
